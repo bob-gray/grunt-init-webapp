e8236d51d9550170747b510987f48f9f1342fa72
Bob Gray <gray.bob98@gmail.com>
2014-01-18 02:28:22 -0500

    adding name to test page title and various lib mappings to runner.js

--- root/tests/runner.js
+++ root/tests/runner.js
@@ -2,7 +2,17 @@ require.config({
 	baseUrl: "../js",
 	paths: {
         spec: "../tests/spec"
-	}
+	},
+    map: {
+        "*": {
+            "css": "../bower_components/require-css/css.min",
+            "solv": "../bower_components/solv/src",
+            "handlebars": "../bower_components/handlebars/handlebars.amd.min",
+            "jquery": "../bower_components/jquery/jquery.min",
+            "jquery.ui": "../bower_components/jqueryui/ui/jquery-ui",
+            "text": "../bower_components/requirejs-text/text"
+        }
+    }
 });
 
 require(
